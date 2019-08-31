3ab7af111c08d6b4e88d75850ded59141a21de8d
Thinh Tran <thinhtran@outlook.com.au>
2019-09-01 06:57:37 +0800

    In process hosting

--- EmployeeManagement/EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/EmployeeManagement/Startup.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
 
 namespace EmployeeManagement
 {
@@ -27,7 +24,7 @@ namespace EmployeeManagement
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(Process.GetCurrentProcess().ProcessName);
             });
         }
     }
