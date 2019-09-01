9d27cd86778eab70a5f954981c24b9090c42af99
Thinh Tran <thinhtran@outlook.com.au>
2019-09-01 16:13:38 +0800

    user fileserver middleware

--- EmployeeManagement/EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/EmployeeManagement/Startup.cs
@@ -30,10 +30,11 @@ namespace EmployeeManagement
                 app.UseDeveloperExceptionPage();
             }
 
-            DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
-            defaultFilesOptions.DefaultFileNames.Clear();
-            defaultFilesOptions.DefaultFileNames.Add("foo.html");
-            app.UseDefaultFiles(defaultFilesOptions);
+            FileServerOptions fileServerOptions = new FileServerOptions();
+            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("foo.html");
+            app.UseFileServer(fileServerOptions);
+
             app.UseStaticFiles();
 
             app.Run(async (context) =>
