3b6309c8ada1de6d6ff24fb91ea4e9ccc4d6121c
Thinh Tran <thinhtran@outlook.com.au>
2019-09-01 16:10:52 +0800

    use default file server options

--- EmployeeManagement/EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/EmployeeManagement/Startup.cs
@@ -30,6 +30,10 @@ namespace EmployeeManagement
                 app.UseDeveloperExceptionPage();
             }
 
+            DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            defaultFilesOptions.DefaultFileNames.Clear();
+            defaultFilesOptions.DefaultFileNames.Add("foo.html");
+            app.UseDefaultFiles(defaultFilesOptions);
             app.UseStaticFiles();
 
             app.Run(async (context) =>
