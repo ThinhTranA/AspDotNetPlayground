10d62bbc59175ddaef21a2a36785e21f01b6caf6
Thinh Tran <thinhtran@outlook.com.au>
2019-09-02 21:21:19 +0800

    setup mvc

--- EmployeeManagement/EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/EmployeeManagement/Startup.cs
@@ -21,6 +21,7 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,13 +32,8 @@ namespace EmployeeManagement
                 app.UseDeveloperExceptionPage();
             }
           
-
             app.UseStaticFiles();
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hosting Enviroment:" + env.EnvironmentName);
-            });
+            app.UseMvcWithDefaultRoute();
 
         
         }
