1829a7f93d74ba6453b7460029f1932f66fb2648
Thinh Tran <thinhtran@outlook.com.au>
2019-09-03 22:38:37 +0800

    add employee model

--- EmployeeManagement/EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
+        private IEmployeeRepository _employeeRepositor;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepositor = employeeRepository;
+        }
         public string Index ()
         {
-            return "Hello from MVC";
+            return _employeeRepositor.GetEmployee(1).Name;
         }
     }
 }
